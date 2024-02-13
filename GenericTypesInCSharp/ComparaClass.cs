3d8ebaac10388ed79d29b7b9523be9c4d5663feb
Filipe Simon <filipe.sena@local.buonny>
2024-02-13 16:10:40 -0300

    Commit inicial

---
+++ GenericTypesInCSharp/ComparaClass.cs
@@ -0,0 +1,37 @@
+﻿namespace GenericTypesInCSharp
+{
+    public class ComparaClass
+    {
+        public void ComparaParametrosDeUmTipo<tipo1>(tipo1 parametro1, tipo1 parametro2) 
+        {
+            bool resultado;
+
+            resultado = parametro1.Equals(parametro2);
+
+            if (resultado)
+            {
+                Console.WriteLine("Os parametros são iguais");
+            }
+            else
+            {
+                Console.WriteLine("Os parametros não são iguais");
+            }
+        }
+
+        public void ComparaParametrosDeDoisTipos<tipo1, tipo2>(tipo1 parametro1, tipo2 parametro2)
+        {
+            bool resultado;
+
+            resultado = parametro1.Equals(parametro2);
+
+            if (resultado)
+            {
+                Console.WriteLine("Os parametros são iguais");
+            }
+            else
+            {
+                Console.WriteLine("Os parametros não são iguais");
+            }
+        }
+    }
+}
---
+++ GenericTypesInCSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace GenericTypesInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComparaClass compara = new ComparaClass();
+
+            int numero1 = 1;
+            int numero2 = 1;
+
+            string string1 = "teste";
+
+            // Aqui o tipo está sendo passado por inferência
+            compara.ComparaParametrosDeUmTipo(numero1, numero2);
+            // Aqui defino o tipo explicitamente
+            compara.ComparaParametrosDeUmTipo<int>(numero1, numero2);
+
+            // A chamada abaixo retorna erro pois não passa valor string no parametro 1
+            compara.ComparaParametrosDeUmTipo<string>(numero1, string1);
+
+            // Aqui o tipo está sendo passado por inferência
+            compara.ComparaParametrosDeDoisTipos(numero1, string1);
+            // Aqui defino o tipo explicitamente
+            compara.ComparaParametrosDeDoisTipos<int, string>(numero1, string1);
+
+            // A chamada abaixo retorna erro pois não passa valor int no parametro 2
+            compara.ComparaParametrosDeDoisTipos<int, string>(numero1, numero2);
+
+            Console.ReadKey();
+        }
+    }
+}
